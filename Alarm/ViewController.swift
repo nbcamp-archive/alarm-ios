f34e244a2c8160ec70142af9693c00eb9b59b9ee
yujinkim1 <yujin7621@me.com>
2023-09-25 23:52:50 +0900

    feat: Alarm 프로젝트 생성(#5)
    - 불필요한 파일은 Git 관리영역에서 제외하기 위해 .gitignore 파일 추가

---
+++ Alarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alarm
+//
+//  Created by Yujin Kim on 2023-09-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
