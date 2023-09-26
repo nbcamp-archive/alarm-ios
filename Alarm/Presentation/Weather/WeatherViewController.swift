7ebb5102f21c730124d46b6e5ecffd9a1f8dd82e
yujinkim1 <yujin7621@me.com>
2023-09-26 14:40:25 +0900

    feat: 날씨 뷰 컨트롤러 생성(#10)

---
+++ Alarm/Presentation/Weather/WeatherViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherViewController.swift
+//  Alarm
+//
+//  Created by Yujin Kim on 2023-09-26.
+//
+
+import UIKit
+
+class WeatherViewController: BaseUIViewController {
+    
+    override func setTitle() {
+        title = "날씨"
+    }
+    
+}
