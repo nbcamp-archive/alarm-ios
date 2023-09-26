92b2f4db89920b43f9c389c101a37101043feb89
yujinkim1 <yujin7621@me.com>
2023-09-26 22:58:46 +0900

    feat: 화면 별 코디네이터 생성 로직 추가(#10)
    - 메인 탭바 컨트롤러로 화면을 전환할 때 코디네이터의 생명 주기를 확인하는 것이 필요

--- Alarm/Application/MainTabBarCoordinator.swift
+++ Alarm/Application/MainTabBarCoordinator.swift
@@ -13,7 +13,7 @@ class MainTabBarCoordinator: Coordinator {
     
     var childCoordinators: [Coordinator]
     
-    var tabBarItems: [MainTabBarItemType]
+    var tabBarItemCases: [MainTabBarItemType]
     var tabBarController: UITabBarController
     
     init(navigationController: UINavigationController) {
@@ -21,39 +21,83 @@ class MainTabBarCoordinator: Coordinator {
         
         self.childCoordinators = [Coordinator]()
         
-        self.tabBarItems = [.weather, .alarm, .stopwatch, .timer]
+        self.tabBarItemCases = MainTabBarItemType.allCases
         self.tabBarController = UITabBarController()
     }
     
     func start() {
-        let viewControllers = tabBarItems.map({ getTabController(tabBarItem: $0) })
+        let tabBarItems = tabBarItemCases.map({ createTabBarItem(from: $0) })
         
-        prepareTabBarController(with: viewControllers)
+        let viewControllers = tabBarItems.map({ createNavigationController(from: $0) })
+        
+        let _ = viewControllers.map({ createChildCoordinator(from: $0) })
+        
+        prepareTabBarController(viewControllers: viewControllers)
+        
+        attachTabBarController()
     }
     
 }
 
 extension MainTabBarCoordinator {
     
-    private func getTabController(tabBarItem: MainTabBarItemType) -> UINavigationController {
+    private func attachTabBarController() {
+        self.navigationController.pushViewController(tabBarController, animated: true)
+    }
+    
+    private func prepareTabBarController(viewControllers: [UIViewController]) {
+        tabBarController.setViewControllers(viewControllers, animated: false)
+        tabBarController.selectedIndex = MainTabBarItemType.weather.toInt()
+        
+        tabBarController.view.backgroundColor = .systemBackground
+        
+        tabBarController.tabBar.isTranslucent = true
+        tabBarController.tabBar.tintColor = UIColor.black
+        tabBarController.tabBar.unselectedItemTintColor = UIColor.systemGray
+    }
+    
+    private func createChildCoordinator(from navigationController: UINavigationController) {
+        let tag = navigationController.tabBarItem.tag
+        guard let tabBarItemType = MainTabBarItemType(index: tag) else { return }
+        
+        switch tabBarItemType {
+        case .weather:
+            let coordinator = WeatherViewCoordinator(navigationController: navigationController)
+            childCoordinators.append(coordinator)
+            coordinator.start()
+        case .alarm:
+            let coordinator = AlarmViewCoordinator(navigationController: navigationController)
+            childCoordinators.append(coordinator)
+            coordinator.start()
+        case .stopwatch:
+            navigationController.navigationBar.isHidden = true
+            let coordinator = StopwatchViewCoordinator(navigationController: navigationController)
+            childCoordinators.append(coordinator)
+            coordinator.start()
+        case .timer:
+            navigationController.navigationBar.isHidden = true
+            let coordinator = TimerViewCoordinator(navigationController: navigationController)
+            childCoordinators.append(coordinator)
+            coordinator.start()
+        }
+    }
+    
+    private func createNavigationController(from tabBarItem: UITabBarItem) -> UINavigationController {
         let navigationController = UINavigationController()
         
-        let tabItem = UITabBarItem(title: tabBarItem.toSymbolName(), image: UIImage(systemName: tabBarItem.toSymbol()), selectedImage: nil)
-        navigationController.tabBarItem = tabItem
-        
-        let coordinator = tabBarItem.getCoordinator(navigationController: navigationController)
-        childCoordinators.append(coordinator)
-        coordinator.start()
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.navigationBar.isTranslucent = true
+        navigationController.tabBarItem = tabBarItem
         
         return navigationController
     }
     
-    private func prepareTabBarController(with viewControllers: [UIViewController]) {
-        tabBarController.setViewControllers(viewControllers, animated: true)
-        tabBarController.selectedIndex = 0
-        tabBarController.tabBar.isTranslucent = true
+    private func createTabBarItem(from tabBarItem: MainTabBarItemType) -> UITabBarItem {
+        let title = tabBarItem.toSymbolName()
+        let symbolName = tabBarItem.toSymbol()
+        let tag = tabBarItem.toInt()
         
-        navigationController.viewControllers = [tabBarController]
+        return UITabBarItem(title: title, image: UIImage(systemName: symbolName), tag: tag)
     }
     
 }
