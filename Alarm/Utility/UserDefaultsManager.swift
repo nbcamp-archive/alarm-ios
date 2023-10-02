ccd5cad96fbbc51538324e3a117233b725ae895a
yujinkim1 <yujin7621@me.com>
2023-10-02 23:19:52 +0900

    refactor: 특정 객체 저장 메서드
    - 특정 객체를 저장하고 이후 다른 객체도 저장하려고 하면 이전 객체가 사라짐
    - 따라서 배열 형태로 저장하는 방식으로 메서드를 수정

--- Alarm/Utility/UserDefaultsManager.swift
+++ Alarm/Utility/UserDefaultsManager.swift
@@ -9,27 +9,31 @@ import Foundation
 
 class UserDefaultsManager {
     
-    static let shared = UserDefaultsManager()
+    static let defaults = UserDefaults.standard
     
     private init() {}
     
-    static func save<T: Codable>(_ object: [T], forKey key: String) {
+    /// UserDefaults에 특정 객체를 저장하는 메서드
+    static func save<T>(_ object: T, forKey key: String) where T: Codable {
+        var existingObject: [T] = load(forKey: key)
+        existingObject.append(object)
+        
         let encoder = JSONEncoder()
-        if let data = try? encoder.encode(object) {
-            UserDefaults.standard.set(data, forKey: key)
+        if let data = try? encoder.encode(existingObject) {
+            defaults.set(data, forKey: key)
         } else {
             print("UserDefaultsManager: 저장하는 것을 실패했습니다.")
         }
     }
-    
-    static func load<T: Codable>(forKey key: String) -> [T] {
-        guard let data = UserDefaults.standard.data(forKey: key) else { return [T]() }
+    /// UserDefaults에 저장되어 있는 특정 객체를 불러오는 메서드
+    static func load<T>(forKey key: String) -> [T] where T: Codable {
+        guard let data = defaults.data(forKey: key) else { return [] }
         
         let decoder = JSONDecoder()
         if let data = try? decoder.decode([T].self, from: data) {
             return data
         } else {
-            return [T]()
+            return []
         }
     }
     
