2b7112eefe9bf51a038f4378dd0e777d027dff34
june-hehe <sdj01071242989@gmail.com>
2023-10-02 13:56:59 +0900

    design: '기타'라벨을 테이블뷰의 기본 첫셀로 수정

--- Alarm/Presentation/Alarm/AlarmViewController.swift
+++ Alarm/Presentation/Alarm/AlarmViewController.swift
@@ -44,24 +44,14 @@ class AlarmViewController: BaseUIViewController {
         
         view.addSubview(tableView)
         
+        tableView.separatorStyle = .singleLine
+        
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-        
-        // 섹션 헤더를 테이블 뷰의 tableHeaderView로 설정
-        let headerView = UIView(frame: CGRect(x: 0, y: 5, width: tableView.frame.width, height: 20))
-        let label = UILabel(frame: CGRect(x: 50, y: 5, width: tableView.frame.width - 32, height: 20))
-        label.text = "기타"
-        label.font = UIFont.systemFont(ofSize: 18.4, weight: .bold)
-        headerView.addSubview(label)
-        
-        tableView.tableHeaderView = headerView
-
-        // 섹션 헤더의 크기를 조절 (헤더 높이만큼)
-        tableView.tableHeaderView?.frame.size.height = 20
         
         // 알람 추가 버튼
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addAlarmButtonTapped))
@@ -75,49 +65,63 @@ class AlarmViewController: BaseUIViewController {
 
 extension AlarmViewController: UITableViewDataSource, UITableViewDelegate {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1 // 첫 번째 섹션만 사용
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return alarms.count
+        return alarms.count + 1 // 알람 수에 1을 더해서 "기타" 라벨 셀을 추가
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 93
+        if indexPath.row == 0 {
+                        return 50 // "기타" 라벨 셀의 높이를 조절
+                    }
+                    return 93
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "AlarmCell")
-        let alarm = alarms[indexPath.row]
-        
-        // 시간 부분 폰트 설정
-        let timeText = NSMutableAttributedString(string: alarm.time)
-        timeText.addAttribute(.font, value: UIFont(name: "SFProDisplay-Light", size: 40) ?? UIFont.systemFont(ofSize: 40), range: NSRange(location: 0, length: 5))
-        
-        // "오전" 또는 "오후" 부분 폰트 설정
-        let amPmRange = (alarm.time as NSString).range(of: "AM,PM")
-        if amPmRange.location != NSNotFound {
-            timeText.addAttribute(.font, value: UIFont(name: "SFProDisplay-Light", size: 20) ?? UIFont.systemFont(ofSize: 20), range: amPmRange)
+        if indexPath.row == 0 {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "HeaderCell")
+            cell.textLabel?.text = "기타"
+            cell.textLabel?.font = UIFont.systemFont(ofSize: 18.4, weight: .bold)
+            return cell
+        } else {
+            let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "AlarmCell")
+            let alarm = alarms[indexPath.row - 1]
+            
+            // 시간 부분 폰트 설정
+            let timeText = NSMutableAttributedString(string: alarm.time)
+            timeText.addAttribute(.font, value: UIFont(name: "SFProDisplay-Light", size: 40) ?? UIFont.systemFont(ofSize: 40), range: NSRange(location: 0, length: 5))
+            
+            // "오전" 또는 "오후" 부분 폰트 설정
+            let amPmRange = (alarm.time as NSString).range(of: "AM,PM")
+            if amPmRange.location != NSNotFound {
+                timeText.addAttribute(.font, value: UIFont(name: "SFProDisplay-Light", size: 20) ?? UIFont.systemFont(ofSize: 20), range: amPmRange)
+            }
+            
+            cell.textLabel?.attributedText = timeText
+            
+            cell.detailTextLabel?.text = alarm.label
+            cell.detailTextLabel?.frame = CGRect(x: 0, y: 0, width: 37, height: 16)
+            cell.detailTextLabel?.font = UIFont(name: "SFProText-Regular", size: 13.8)
+            
+            cell.selectionStyle = .none
+            
+            // 토글 스위치 추가
+            let switchView = UISwitch()
+            switchView.isOn = false // 꺼진 상태로 설정
+            switchView.addTarget(self, action: #selector(toggleSwitch(_:)), for: .valueChanged)
+            cell.accessoryView = switchView // 셀에 토글 스위치 추가
+            
+            return cell
         }
-        
-        cell.textLabel?.attributedText = timeText
-        
-        cell.detailTextLabel?.text = alarm.label
-        cell.detailTextLabel?.frame = CGRect(x: 0, y: 0, width: 37, height: 16)
-        cell.detailTextLabel?.font = UIFont(name: "SFProText-Regular", size: 13.8)
-        
-        cell.selectionStyle = .none
-        
-        // 토글 스위치 추가
-        let switchView = UISwitch()
-        switchView.isOn = false // 꺼진 상태로 설정
-        switchView.addTarget(self, action: #selector(toggleSwitch(_:)), for: .valueChanged)
-        cell.accessoryView = switchView // 셀에 토글 스위치 추가
-        
-        return cell
     }
     
     @objc func toggleSwitch(_ sender: UISwitch) {
         if let cell = sender.superview as? UITableViewCell,
            let indexPath = tableView.indexPath(for: cell) {
-            let alarm = alarms[indexPath.row]
+            let alarm = alarms[indexPath.row - 1]
             alarmSwitchValueChanged(isOn: sender.isOn, forAlarm: alarm)
         }
     }
