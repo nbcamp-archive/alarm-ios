a79366d1eec98dc332620fa2037be6e38f60e3b6
yujinkim1 <yujin7621@me.com>
2023-10-03 20:46:56 +0900

    fix: 사용자 알림 예약 오류 해결

--- Alarm/Utility/AlarmScheduler.swift
+++ Alarm/Utility/AlarmScheduler.swift
@@ -12,33 +12,44 @@ class AlarmScheduler {
     
     private static let notificationCenter = UNUserNotificationCenter.current()
     
-    static func scheduleAlarms() {
+    static func registAlarms() {
+        notificationCenter.removeAllPendingNotificationRequests()
+        
         let alarmGroup: [Alarm] = UserDefaultsManager.load(forKey: UserDefaultsManager.alarmGroupKey)
         
         for alarm in alarmGroup {
-            for dayOfWeek in alarm.dayOfWeekdays {
-                var dateComponents = DateComponents()
-                dateComponents.hour = alarm.hour
-                dateComponents.minute = alarm.minute
-                dateComponents.weekday = dayOfWeek + 2
-                
-                let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-                
-                let content = UNMutableNotificationContent()
-                content.title = "알람"
-                content.body = alarm.label
-                content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: alarm.tone.filename))
-                
-                let requestIdentifier = "\(alarm.uuid.uuidString)-\(dayOfWeek)"
-                let request = UNNotificationRequest(identifier: requestIdentifier, content: content, trigger: trigger)
-                
-                notificationCenter.add(request) { (error) in
-                    if let error = error {
-                        print("알람 등록을 실패했습니다: \(error)")
-                    } else {
-                        print("알람이 성공적으로 예약되었습니다.")
+            if alarm.isEnabled {
+                for dayOfWeek in alarm.dayOfWeekdays {
+                    var dateComponents = DateComponents()
+                    dateComponents.hour = alarm.hour
+                    dateComponents.minute = alarm.minute
+                    
+                    let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+                    
+                    let content = UNMutableNotificationContent()
+                    content.title = "알람"
+                    content.body = alarm.label
+                    content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "daydreaming.caf"))
+                    
+                    let requestIdentifier = "\(alarm.uuid.uuidString)-\(dayOfWeek)"
+                    let request = UNNotificationRequest(identifier: requestIdentifier, content: content, trigger: trigger)
+                    
+                    notificationCenter.add(request) { (error) in
+                        if let error = error {
+                            print("알람 등록을 실패했습니다: \(error)")
+                        } else {
+                            print("알람이 성공적으로 예약되었습니다.")
+                        }
                     }
                 }
+            }
+        }
+    }
+    
+    static func checkScheduledAlarms() {
+        notificationCenter.getPendingNotificationRequests { (requests) in
+            for request in requests {
+                print(request)
             }
         }
     }
