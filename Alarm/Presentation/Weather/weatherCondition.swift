535943a8b2533b8ad5abc8db2b0d2f6a1ffe76ad
kiakim01 <hnmpot0318@naver.com>
2023-10-05 18:46:53 +0900

    every case color setting

--- Alarm/Presentation/Weather/weatherCondition.swift
+++ Alarm/Presentation/Weather/weatherCondition.swift
@@ -53,23 +53,28 @@ extension WeatherViewController{
         let yoroke = Temp(initailValue: weatherConditon)
         if let tempCase = yoroke{
             switch tempCase {
-            case .snow:
-                weatherImage.image = UIImage(named: "snow")
-            case .clear:
-                weatherImage.image = UIImage(named: "clear")
-                HEX = "d90429" // [Bug3] : 날씨별 배경
-//                gradient.animateGradient(hex: HEX)
+                
             case .thunderstorm: //뇌우
                 weatherImage.image = UIImage(named: "thunderstorm")
+                HEX = "031d44" //navi
             case .drizzle: //가랑비
                 weatherImage.image = UIImage(named: "drizzle")
+                HEX = "5390d9" //blue
             case .rain:
                 weatherImage.image = UIImage(named: "rain")
-                
+                HEX = "5390d9" //blue
+            case .snow:
+                weatherImage.image = UIImage(named: "snow")
+                HEX = "64b878" //green
             case .atmosphere:
                 weatherImage.image = UIImage(named: "atmosphere")
+                HEX = "94d2bd" //grayEmerald
+            case .clear:
+                weatherImage.image = UIImage(named: "clear")
+                HEX = "4895ef" // skyblue
             case .clouds:
                 weatherImage.image = UIImage(named: "clouds")
+                HEX = "5390d9" //blue
             }
         } else {
             print("No Icon")
